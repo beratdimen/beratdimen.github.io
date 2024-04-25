c4aca5679aad4cddda8cb7d8d8ede85785ead75f
Berat Dimen <beratefedimen@hotmail.com>
2024-04-25 10:11:15 +0300

    proje ve hakkımda scroll to

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var projelerLinkleri = document.querySelectorAll(".yollar li:nth-child(3) a");
-  var projelerBolgesi = document.querySelector(".projeler");
-
-  projelerLinkleri.forEach(function(link) {
-    link.addEventListener("click", function(event) {
-      event.preventDefault();
-      var projelerBolgePos = projelerBolgesi.offsetTop;
-      window.scrollTo({
-        top: projelerBolgePos,
-        behavior: "smooth"
-      });
-    });
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
   var hakkimdaLink = document.querySelector(".yollar li:nth-child(2) a");
+  var projelerLink = document.querySelector(".yollar li:nth-child(3) a");
   var hakkimdaBolgesi = document.querySelector(".hakkimda");
+  var projelerBolgesi = document.querySelector(".projeler");
 
   hakkimdaLink.addEventListener("click", function(event) {
     event.preventDefault();
     var hakkimdaBolgePos = hakkimdaBolgesi.offsetTop;
     window.scrollTo({
       top: hakkimdaBolgePos,
+      behavior: "smooth"
+    });
+  });
+
+  projelerLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    var projelerBolgePos = projelerBolgesi.offsetTop;
+    window.scrollTo({
+      top: projelerBolgePos,
       behavior: "smooth"
     });
   });
