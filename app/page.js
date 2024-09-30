0ba9db3d2f80dfa8f285fa1d47cab0013ceee2b8
Berat Dimen <beratefedimen@hotmail.com>
2024-09-30 10:40:36 +0300

    experience kısmı html sayfası oluşturuldu

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import Experience from "@/components/experience";
 import Slider from "@/components/slider";
 
 export default function Home() {
   return (
     <>
       <Slider />
+      <Experience />
     </>
   );
 }
